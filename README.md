af11ee8631a0b7119bc8a7bdf6ccfc2272bcb969
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2024-01-03 13:32:19 -0500

    added Cfonts and updated README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# employee-tracker
+# Employee Tracker
+
+## Description
+  This is an interface that allows non-developers to easily view and interact with information stored in databases. This content management system (CMS) is a command-line application using Node.js, inquirer, and MySQL. Seed data is fictional and employee names are friends and previous employers.
+
+  ## Requirements
+  Node.js, inquirer, MySQL  
+
+  ## Usage
+  1) Open the employee tracker.
+  2) Open the "intergrated terminal" on "server.js" and enter "node server.js"
+  3) Use the "UP" and "DOWN" arrow keys to navigate the menu.
+  4) Click "enter" on the desired option.
+  5) Follow the command-line prompts. 
+
+
+  ## Built With
+  * Dynamic JavaScript
+  * Node.js v20.10.0
+  * inquirer v8.2.4
+  * MySQL2 
+  * Console.table v0.10.0
+  * Cfonts v3.1.1
+
+  ## Credits
+  Youtube THOSCALLE - https://www.youtube.com/watch?v=m9CQxR0AfiQ for help with the bonus of viewing the total utilized budget.
+  
+  ## License
+  N/A
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cfonts = require('cfonts');
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -14,6 +15,22 @@ db.connect((err) => {
   console.log(`Connected to the database.`);
   // start application
   initiate();
+});
+
+// Function to start the application of CFONT 
+cfonts.say('Employee \nTracker', {
+	font: 'block',              // define the font face
+	align: 'left',              // define text alignment
+	colors: ['green'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment cfonts is being executed in
 });
 
 // function to start the application
